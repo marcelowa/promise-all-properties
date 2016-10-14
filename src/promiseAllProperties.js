65ea1f88b5be65ed0855f48af49c8475b40550cc
Marcelo Waisman <marcelo.waisman@gmail.com>
2016-10-14 11:41:47 +0300

    [INITIAL]

---
+++ src/promiseAllProperties.js
@@ -0,0 +1,36 @@
+/**
+ * Receives an object with promise containing properties and returns a promise that resolves to an object
+ * with the same properties containing the resolved values
+ * @param  {Object} promisesObject  the input object with a promise in each property
+ * @return {Pormise}  a promise that resolved to an object with the same properties containing the resolved values
+ */
+export default function promiseAllProperties(promisesObject) {
+  if (typeof(promisesObject) !== 'object') {
+    return Promise.reject('The input argument must be of type Object');
+  }
+
+  const promisesArray = [];
+  const propertiesIndex = [];
+
+  for (const property in promisesObject) {
+    const promise = promisesObject[property];
+    if (!(promise instanceof Promise)) {
+      return Promise.reject(`The property ${property} must contain a promise`)
+    }
+
+    promisesArray.push(promise);
+    propertiesIndex.push(property);
+  }
+
+  return Promise.all(promisesArray).then((result) => {
+    const resolvedObject = {};
+
+    result.forEach((value, index) => {
+      const property = propertiesIndex[index];
+
+      resolvedObject[property] = value;
+    });
+    return resolvedObject;
+  });
+
+}
---
+++ test/promiseAllProperties.spec.js
@@ -0,0 +1,55 @@
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import promiseAllProperties from '../src/promiseAllProperties';
+
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+describe('promiseAllProperties', () => {
+  describe('validation rejections', () => {
+    it('should reject if not receiving an object', () => {
+      return expect(promiseAllProperties()).to.be.rejectedWith('The input argument must be of type Object');
+    });
+
+    it('should reject if the input object contains a non promise property', () => {
+      return expect(promiseAllProperties({x: 1})).to.be.rejectedWith('The property x must contain a promise');
+    });
+
+  });
+
+  describe('the input is an empty object', () => {
+    it('should return a promise', () => {
+      expect(promiseAllProperties({})).to.be.instanceOf(Promise);
+    });
+
+    it('should return a promise the resolves to an object', () => {
+      return expect(promiseAllProperties({})).to.eventually.be.an('object');
+    });
+  });
+
+  describe('input with promises that should resolve', () => {
+    it('should resolve to an object with resolved values instead', () => {
+      const promisesObject = {
+        firstPromise : Promise.resolve('result of first promise'),
+        secondPromise : Promise.resolve('result of second promise')
+      }
+      const promise = promiseAllProperties(promisesObject);
+      return expect(promise).to.eventually.contain({
+          firstPromise : 'result of first promise',
+          secondPromise : 'result of second promise'
+      });
+    });
+  });
+
+  describe('input with promises that should resolve', () => {
+    it('should be rejected if at least one of the promises is rejected', () => {
+      const promisesObject = {
+        firstPromise : Promise.resolve('result of first promise'),
+        secondPromise : Promise.reject('error in the second promise')
+      }
+      const promise = promiseAllProperties(promisesObject);
+      return expect(promise).to.be.rejectedWith('error in the second promise');
+    });
+  });
+
+});
